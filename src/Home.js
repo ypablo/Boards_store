fac74fafb10956d1ff2a766f7b4b3e4c8f36024d
ypablo <yfedora@gmail.com>
2019-07-25 17:54:05 +0100

    Str2

--- src/Home.js
+++ src/Home.js
@@ -7,31 +7,47 @@ import Pagination from "./Pagination"
 class Home extends React.Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            numberOfPages: 2,
+            numberOfProducts: jsonBoards.length,
+            setNumberOfProducts: [],
+            recordsPerPage: 4,
+            setRecordsPerPage: 4,
+            currentPage: 1,
+            setCurrentPage: 1
+        }
     }
+
+
     render() {
         const navStyle = {
             height: "550px",
             width: "300px",
             margin: "5px",
-            border: "1px solid #000" 
+            border: "1px solid #000"
         }
+
+        const iLastRecord = this.state.currectpage * this.state.recordsPerPage;
+        const iFirstRecord = iLastRecord - this.state.recordsPerPage;
+        const currentRecords = this.state.numberOfProducts.slice(iFirstRecord, iLastRecord);
+
+
 
         return (
             <div>
-            <div className="home">          
-                {jsonBoards.map((item, index) => {
-                    return <div key={index}>
-                                <h1>{item.name}</h1>
-                                <p>Price: {item.price}</p>
-                                <img style={navStyle} src={item.img} alt=""/>
-                                <p>{item.last}</p>
-                                <p>{item.new}</p>
-                            </div>
-                            })}              
-            </div>
-                <Pagination/>
-            </div>
+                <div className="home">
+                    {jsonBoards.map((item, index) => {
+                        return <div key={index}>
+                            <h1>{item.name}</h1>
+                            <p>Price: {item.price}</p>
+                            <img style={navStyle} src={item.img} alt="" />
+                            <p>{item.last}</p>
+                            <p>{item.new}</p>
+                        </div>
+                    })}
+                </div>
+                <Pagination />
+            </div >
         )
     }
 }
