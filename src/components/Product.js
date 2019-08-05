fdf15bc4e1d527705052e7c9145c766d98d01c35
ypablo <yfedora@gmail.com>
2019-08-05 18:35:50 +0100

    basket

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,32 +7,27 @@ import { detailProduct } from '../detailProduct'
 export default class Product extends Component {
     render() {
         return (
-            <ProductConsumer className="container">
+            <ProductConsumer>
                 {(value) => {
                     const { id, title, img, info, price, inCart } = value.detailProduct;
                     return (
-                        <div className="ProductContainer">
-
-                            {/* title */}
-                            <div className="ProductTitle">
-                                <h1>{title}</h1>
-                            </div>
-
-                            <div className="ProductImgInfo">
-                                {/* img */}
-                                <div className="ProductImg"><img src={img} alt="product" /></div>
+                        <div className="ProductContainer" >
+                            {/* img */}
+                            <div className="ProductImg"><img className="ProdImg" src={img} alt="product" /></div>
+                            <div className="ProductSummary">
+                                {/* title */}
+                                <div className="ProductTitle"><h1>{title}</h1></div>
                                 {/* info */}
                                 <div className="ProductInfo">Description: {info}</div>
+                                {/* price */}
+                                <div className="ProductPrice"><h3>Price: {price}</h3></div>
+                                <div className="ProductButtons">
+                                    <Link to="/"><button className="BackProduct-btn">Back to products</button></Link>
+                                    <button className="Basket-btn" disabled={inCart ? true : false} onClick={() => {
+                                        value.addToCart(id)
+                                    }}>{inCart ? 'InCart' : 'Add to basket'}</button>
+                                </div>
                             </div>
-                            {/* price */}
-                            <div><h1>Price: {price}</h1></div>
-                            <div>
-                                <Link to="/"><button className="BackProduct-btn">Back to products</button></Link>
-                                <button className="Basket-btn" disabled={inCart ? true : false} onClick={() => {
-                                    value.addToCart(id)
-                                }}>{inCart ? 'InCart' : 'Add to basket'}</button>
-                            </div>
-
                         </div>
                     )
                 }}
